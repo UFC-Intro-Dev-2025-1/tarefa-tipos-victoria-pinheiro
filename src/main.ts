4e1d137a61d3e3cb8963966ae55ebe644bbf3674
Victória Maria dos Santos Pinheiro <victoriamariaspinheiro@gmail.com>
2025-05-28 00:16:52 +0000

    feat:solução de tarefa Tipos

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-// Escreva seu código aqui!
+const number = "2";
+const string = "Numero de portas no laboratório";
+const estouProgramando = true;
+const fiosDeCabeloNaMinhaCabeça = undefined
+const objeto = {estojo: "azul, brilhante, pequeno"};
